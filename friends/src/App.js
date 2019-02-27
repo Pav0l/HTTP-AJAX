9b638df558e068cd7094d65cfd02f6d287902810
Pav0l <pavol.karas@gmail.com>
2019-02-27 13:34:15 +0100

    Setup fetch method with standard JS

--- friends/src/App.js
+++ friends/src/App.js
@@ -3,13 +3,69 @@ import styled from 'styled-components';
 import axios from 'axios';
 
 export default class App extends Component {
+  state = {
+    friends: null,
+    loading: false,
+    error: null,
+  }
+
+  componentDidMount() {
+    this.fetchFriends();
+  }
+
+  loaderStart = () => this.setState({ loading: true });
+
+  loaderFinish = () => this.setState({ loading: false });
+
+  fetchFriends = () => {
+    this.loaderStart();
+    fetch('http://localhost:5000/friends')
+      .then(data => data.json())
+      .then(friends => {
+        this.setState({ friends });
+        this.loaderFinish();
+      })
+      .catch(this.setError);
+  }
+
+  setError = (error) => {
+    this.setState({ error });
+  }
 
   render() {
+
+    if (this.state.loading) {
+      return (
+        <StyledWrapper>
+          Loading some friends for your lonely soul...
+        </StyledWrapper>
+      );
+    }
+
     return (
-      <div>
-        Hello world!
-      </div>
+      <StyledWrapper>
+        {
+          this.state.friends && this.state.friends.map(friend => (
+            <div>
+              <span>{friend.id} - </span>
+              <span>{friend.name} - </span>
+              <span>{friend.age} - </span>
+              <span>{friend.email}</span>
+            </div>
+          ))
+        }
+      </StyledWrapper>
     );
   }
 }
 
+const StyledWrapper = styled.div`
+  font-size: 2rem;
+
+  div {
+    span {
+      color: red;
+      font-size: 2rem;
+    }
+  }
+`;
