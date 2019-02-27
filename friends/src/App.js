1c03e421c84fa232fa0eac71eae173a4acc3ca45
Pav0l <pavol.karas@gmail.com>
2019-02-27 13:09:44 +0100

    Initial commit, CRA

---
+++ friends/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import axios from 'axios';
+
+export default class App extends Component {
+
+  render() {
+    return (
+      <div>
+        Hello world!
+      </div>
+    );
+  }
+}
+
